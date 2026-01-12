24aea918686d535807c243eeec18bd0ca543dd19
fightersubmarine <alexsokcolow968968@gmail.com>
2026-01-12 20:55:51 +0300

    Initial Commit

---
+++ learn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learn
+//
+//  Created by Александр Соколов on 12.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
